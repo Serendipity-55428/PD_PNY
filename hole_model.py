0fb1732b71c5d58de7a46112c0ac40a42964e1cf
xiaosonggege <1243049371@qq.com>
2019-07-27 11:37:09 +0800

    加入了三个模型整体对最优半径的预测模型组合

--- hole_model.py
+++ hole_model.py
@@ -13,6 +13,8 @@ import numpy as np
 import pandas as pd
 import os
 from Saving_model_pb import SaveImport_model, use_pb, import_ops
+from inputdata import LoadFile, guiyi
+from dataset_making import fft_transformer
 
 def first_check(input):
     '''
@@ -43,7 +45,7 @@ def second_check1(input):
     :param input: 输入特征向量(矩阵)/标签
     :return: 具体类别, numpy.ndarray
     '''
-    dict1 = {0:7.17, 1:17.93, 2:20}
+    dict1 = {0:7.174541, 1:17.936353, 2:20.0}
     pb_file_path = r'/home/xiaosong/桌面/model/full_model_1'
     g2 = tf.Graph()
     with g2.as_default():
@@ -71,7 +73,8 @@ def second_check2(input):
     :param input: 输入特征向量(矩阵)/标签
     :return: 具体类别, numpy.ndarray
     '''
-    dict2 = {0:0.05, 1:3.26, 2:4.48, 3:23, 4:35, 5:71, 6:107, 7:143, 8:179, 9:215, 10:251}
+    dict2 = {0:0.05, 1:3.261155, 2:4.484088, 3:23.915137, 4:35.872705, 5:71.745411,
+             6:107.618116, 7:143.490822, 8:179.363527, 9:215.236233, 10:251.108938}
     pb_file_path = r'/home/xiaosong/桌面/model/full_model_2'
     g3 = tf.Graph()
     with g3.as_default():
@@ -100,23 +103,29 @@ def check(input):
     :return: 最优半径矩阵, numpy.ndarray
     '''
     r_classify = first_check(input=input) #得到初级分类器的三类别标记, 分别为0, 1, 2
+    # print(r_classify)
     #将初分类结果添加到输入数据集最后一列
     input_1 = pd.DataFrame(data=input, columns=[str(i) for i in range(input.shape[-1])])
     input_1['%s' % input.shape[-1]] = r_classify
+    # print(input_1.values.shape)
     #筛选出经过初分类器后类别标记为0的所有数据
     input_firstc = input_1.loc[input_1['%s' % input.shape[-1]]==0]
-    input_firstc.drop(columns=['%s' % input.shape[-1]])
+    # print(input_firstc.values.shape)
+    input_firstc = input_firstc.drop(columns=['%s' % input.shape[-1]])
     input_firstc = input_firstc.values
+    # print(input_firstc.shape)
     r_finally1 = np.ones(dtype=np.float32, shape=[input_firstc.shape[0], 1]) * 0.01
     #筛选出经过初分类器后类别标记为1的所有数据
     input_secondc1 = input_1.loc[input_1['%s' % input.shape[-1]]==1]
-    input_secondc1.drop(columns=['%s' % input.shape[-1]])
+    # print(input_secondc1.shape)
+    input_secondc1 = input_secondc1.drop(columns=['%s' % input.shape[-1]])
     input_secondc1 = input_secondc1.values
+    # print(input_secondc1.shape)
     #放入三分类器中进一步分类
     r_finally2 = np.array(second_check1(input_secondc1))[:, np.newaxis]
     #筛选出经过初分类器后类别标记为2的所有数据
     input_secondc2 = input_1.loc[input_1['%s' % input.shape[-1]]==2]
-    input_secondc2.drop(columns=['%s' % input.shape[-1]])
+    input_secondc2 = input_secondc2.drop(columns=['%s' % input.shape[-1]])
     input_secondc2 = input_secondc2.values
     #放入11分类器中进一步分类
     r_finally3 = np.array(second_check2(input_secondc2))[:, np.newaxis]
@@ -126,11 +135,18 @@ def check(input):
     return r_finally, r_real
 
 if __name__ == '__main__':
-    input = np.arange(105)[np.newaxis, :]
-    r_finally, r_real = check(input=input)
+    p = r'/home/xiaosong/桌面/PNY_all.pickle'
+    input = LoadFile(p=p)
+    np.random.shuffle(input)
+    dataset_4feature, dataset_dense, label = input[:, :4], input[:, 4:-1], input[:, -1][:, np.newaxis]
+    dataset_fft = fft_transformer(dataset_dense, 100)
+    dataset = np.hstack((dataset_4feature, dataset_fft, label))
+    dataset_guiyi_2 = guiyi(dataset)
+    print(dataset_guiyi_2.shape)
+    r_finally, r_real = check(input=dataset_guiyi_2[:5100, :])
     r_1 = np.where(np.abs(r_finally-r_real)<1e-2, 1, 0)
     r_sum = np.sum(r_1)
-    acc = r_sum / r_sum.shape[0]
+    acc = r_sum / r_1.shape[0]
     print(acc)
 
 
