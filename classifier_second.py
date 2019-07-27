237d670a8841a1fa327de780b6599435b630ed5e
xiaosonggege <1243049371@qq.com>
2019-07-27 09:44:08 +0800

    y

--- classifier_second.py
+++ classifier_second.py
@@ -81,7 +81,7 @@ class Resnet:
         relu = tf.keras.layers.ReLU(name=self.__name)(combination)
         return relu
 
-def subnet_1(x_f, x_l, is_training):
+def subnet_resnet_1(x_f, x_l, is_training):
     '''
     三分类残差网络层
     :param x_f: 4个与密度无关特征
@@ -98,13 +98,16 @@ def subnet_1(x_f, x_l, is_training):
         res1 = resnet.resnet_2layers()
 
         resnet2 = Resnet(x=res1, filters=128, kernel_size=[3, 3], name='resnet2')
-        res2 = resnet2.resnet_3layers()
+        res2 = resnet2.resnet_2layers()
 
         resnet3 = Resnet(x=res2, filters=256, kernel_size=[3, 3], name='resnet3')
-        res3 = resnet3.resnet_3layers()
+        res3 = resnet3.resnet_2layers()
 
         resnet4 = Resnet(x=res3, filters=256, kernel_size=[3, 3], name='resnet4')
         res4 = resnet4.resnet_2layers()
+
+        # resnet5 = Resnet(x=res4, filters=128, kernel_size=[3, 3], name='resnet5')
+        # res5 = resnet5.resnet_2layers()
 
         flat = tf.keras.layers.Flatten(name='flat')(res4)
     with tf.name_scope('sub_dnn'):
@@ -123,32 +126,42 @@ def subnet_1(x_f, x_l, is_training):
         output2 = tf.keras.activations.softmax(x=output)
         return output2
 
-def subnet_2(x_f, x_l, is_training):
+def subnet_cnnlstm_1(x_f, x_l, is_training):
     '''
-    11分类残差网络层
+    三分类残差网络层
     :param x_f: 4个与密度无关特征
     :param x_l: 100个密度特征
     :param is_training: 指示是否在训练
     :return: 神经网络最后输出
     '''
-    with tf.name_scope('sub_cnn'):
-        x_reshape = tf.reshape(tensor=x_l, shape=[-1, 10, 10, 1], name='x_reshape')
-        conv = tf.keras.layers.Conv2D(filters=16, kernel_size=[5, 5], padding='same', activation=tf.nn.relu,
-                                       kernel_initializer=tf.keras.initializers.TruncatedNormal, name='conv1')(x_reshape)
-        pool1 = tf.keras.layers.MaxPool2D(pool_size=[2, 2], strides=2, padding='same', name='pool1')(conv)
-
-        resnet = Resnet(x=pool1, filters=32, kernel_size=[3, 3], name='resnet1')
-        res1 = resnet.resnet_2layers()
-
-        resnet2 = Resnet(x=res1, filters=128, kernel_size=[3, 3], name='resnet2')
-        res2 = resnet2.resnet_3layers()
-
-        resnet3 = Resnet(x=res2, filters=256, kernel_size=[3, 3], name='resnet3')
-        res3 = resnet3.resnet_2layers()
-
-        flat = tf.keras.layers.Flatten(name='flat')(res3)
-    with tf.name_scope('sub_dnn'):
-        x_dnn = tf.concat(values=[flat, x_f], axis=1)
+    with tf.name_scope('cnn'):
+        x_conv = tf.reshape(tensor=x_l, shape=[-1, 10, 10, 1], name='x_conv')
+        conv1 = tf.keras.layers.Conv2D(filters=32, kernel_size=[5, 5], padding='same', activation=tf.nn.relu,
+                                       kernel_initializer=tf.keras.initializers.TruncatedNormal, name='conv1')(x_conv)
+        pool1 = tf.keras.layers.MaxPool2D(pool_size=[2, 2], strides=2, padding='same', name='pool1')(conv1)
+        bn1 = tf.keras.layers.BatchNormalization(name='bn_input1')
+        bn_input1 = bn1(inputs=pool1, training=is_training)
+        # 添加bn层节点依赖
+        tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, bn1.updates)
+        conv2 = tf.keras.layers.Conv2D(filters=64, kernel_size=[3, 3], padding='same', activation=tf.nn.relu,
+                                       kernel_initializer=tf.keras.initializers.TruncatedNormal, name='conv2')(bn_input1)
+        # conv3 = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], padding='same', activation=tf.nn.relu,
+        #                                kernel_initializer=tf.keras.initializers.TruncatedNormal, name='conv3')(conv2)
+        pool2 = tf.keras.layers.MaxPool2D(pool_size=[2, 2], strides=2, padding='same', name='pool2')(conv2)
+        bn2 = tf.keras.layers.BatchNormalization(name='bn_input2')
+        bn_input2 = bn2(inputs=pool2, training=is_training)
+        # 添加bn层节点依赖
+        tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, bn2.updates)
+        conv4 = tf.keras.layers.Conv2D(filters=64, kernel_size=[2, 2], padding='same', activation=tf.nn.relu,
+                                       kernel_initializer=tf.keras.initializers.TruncatedNormal, name='conv4')(bn_input2)
+        flat1 = tf.keras.layers.Flatten(name='flat1')(conv4)
+    with tf.name_scope('rnn'):
+        x_lstm = tf.reshape(tensor=flat1, shape=[-1, 24, 24], name='x_lstm')
+        # lstm1 = tf.keras.layers.LSTM(units=128, dropout=0.8, return_sequences=True, name='lstm1')(x_lstm)
+        lstm2 = tf.keras.layers.LSTM(units=128, dropout=0.8, return_sequences=False, name='lstm2')(x_lstm)
+        flat2 = tf.keras.layers.Flatten(name='flat2')(lstm2)
+    with tf.name_scope('dnn'):
+        x_dnn = tf.concat(values=[flat2, x_f], axis=1)
         x_fc1 = tf.keras.layers.Dense(units=100, activation=tf.nn.relu, use_bias=True,
                                       kernel_initializer=tf.keras.initializers.TruncatedNormal,
                                       bias_initializer=tf.keras.initializers.TruncatedNormal, name='x_fc1')(x_dnn)
@@ -160,8 +173,103 @@ def subnet_2(x_f, x_l, is_training):
         output = tf.keras.layers.Dense(units=3, activation=tf.nn.relu, use_bias=True,
                                        kernel_initializer=tf.keras.initializers.TruncatedNormal,
                                        bias_initializer=tf.keras.initializers.TruncatedNormal, name='output')(x_dpt2)
+        output = tf.keras.activations.softmax(x=output)
+    return output
+
+def subnet_resnet_2(x_f, x_l, is_training):
+    '''
+    11分类残差网络层
+    :param x_f: 4个与密度无关特征
+    :param x_l: 100个密度特征
+    :param is_training: 指示是否在训练
+    :return: 神经网络最后输出
+    '''
+    with tf.name_scope('sub_cnn'):
+        x_reshape = tf.reshape(tensor=x_l, shape=[-1, 10, 10, 1], name='x_reshape')
+        conv = tf.keras.layers.Conv2D(filters=64, kernel_size=[5, 5], padding='same', activation=tf.nn.relu,
+                                      kernel_initializer=tf.keras.initializers.TruncatedNormal, name='conv1')(x_reshape)
+        pool1 = tf.keras.layers.MaxPool2D(pool_size=[2, 2], strides=2, padding='same', name='pool1')(conv)
+        resnet = Resnet(x=pool1, filters=64, kernel_size=[3, 3], name='resnet1')
+        res1 = resnet.resnet_2layers()
+
+        resnet2 = Resnet(x=res1, filters=128, kernel_size=[3, 3], name='resnet2')
+        res2 = resnet2.resnet_2layers()
+
+        resnet3 = Resnet(x=res2, filters=128, kernel_size=[3, 3], name='resnet3')
+        res3 = resnet3.resnet_2layers()
+
+        resnet4 = Resnet(x=res3, filters=256, kernel_size=[3, 3], name='resnet4')
+        res4 = resnet4.resnet_2layers()
+
+        resnet5 = Resnet(x=res4, filters=256, kernel_size=[3, 3], name='resnet5')
+        res5 = resnet5.resnet_2layers()
+
+        flat = tf.keras.layers.Flatten(name='flat')(res5)
+    with tf.name_scope('sub_dnn'):
+        x_dnn = tf.concat(values=[flat, x_f], axis=1)
+        x_fc1 = tf.keras.layers.Dense(units=100, activation=tf.nn.relu, use_bias=True,
+                                      kernel_initializer=tf.keras.initializers.TruncatedNormal,
+                                      bias_initializer=tf.keras.initializers.TruncatedNormal, name='x_fc1')(x_dnn)
+        x_dpt1 = tf.keras.layers.Dropout(rate=0.2, name='x_dpt1')(inputs=x_fc1, training=is_training)
+        x_fc2 = tf.keras.layers.Dense(units=200, activation=tf.nn.relu, use_bias=True,
+                                      kernel_initializer=tf.keras.initializers.TruncatedNormal,
+                                      bias_initializer=tf.keras.initializers.TruncatedNormal, name='x_fc2')(x_dpt1)
+        x_dpt2 = tf.keras.layers.Dropout(rate=0.2, name='x_dpt2')(inputs=x_fc2, training=is_training)
+        output = tf.keras.layers.Dense(units=11, activation=tf.nn.relu, use_bias=True,
+                                       kernel_initializer=tf.keras.initializers.TruncatedNormal,
+                                       bias_initializer=tf.keras.initializers.TruncatedNormal, name='output')(x_dpt2)
         output2 = tf.keras.activations.softmax(x=output)
         return output2
+
+def subnet_cnnlstm_2(x_f, x_l, is_training):
+    '''
+    11分类残差网络层
+    :param x_f: 4个与密度无关特征
+    :param x_l: 100个密度特征
+    :param is_training: 指示是否在训练
+    :return: 神经网络最后输出
+    '''
+    with tf.name_scope('cnn'):
+        x_conv = tf.reshape(tensor=x_l, shape=[-1, 10, 10, 1], name='x_conv')
+        conv1 = tf.keras.layers.Conv2D(filters=32, kernel_size=[5, 5], padding='same', activation=tf.nn.relu,
+                                       kernel_initializer=tf.keras.initializers.TruncatedNormal, name='conv1')(x_conv)
+        pool1 = tf.keras.layers.MaxPool2D(pool_size=[2, 2], strides=2, padding='same', name='pool1')(conv1)
+        bn1 = tf.keras.layers.BatchNormalization(name='bn_input1')
+        bn_input1 = bn1(inputs=pool1, training=is_training)
+        # 添加bn层节点依赖
+        tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, bn1.updates)
+        conv2 = tf.keras.layers.Conv2D(filters=64, kernel_size=[3, 3], padding='same', activation=tf.nn.relu,
+                                       kernel_initializer=tf.keras.initializers.TruncatedNormal, name='conv2')(bn_input1)
+        # conv3 = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], padding='same', activation=tf.nn.relu,
+        #                                kernel_initializer=tf.keras.initializers.TruncatedNormal, name='conv3')(conv2)
+        pool2 = tf.keras.layers.MaxPool2D(pool_size=[2, 2], strides=2, padding='same', name='pool2')(conv2)
+        bn2 = tf.keras.layers.BatchNormalization(name='bn_input2')
+        bn_input2 = bn2(inputs=pool2, training=is_training)
+        # 添加bn层节点依赖
+        tf.add_to_collection(tf.GraphKeys.UPDATE_OPS, bn2.updates)
+        conv4 = tf.keras.layers.Conv2D(filters=64, kernel_size=[2, 2], padding='same', activation=tf.nn.relu,
+                                       kernel_initializer=tf.keras.initializers.TruncatedNormal, name='conv4')(bn_input2)
+        flat1 = tf.keras.layers.Flatten(name='flat1')(conv4)
+    with tf.name_scope('rnn'):
+        x_lstm = tf.reshape(tensor=flat1, shape=[-1, 24, 24], name='x_lstm')
+        # lstm1 = tf.keras.layers.LSTM(units=128, dropout=0.8, return_sequences=True, name='lstm1')(x_lstm)
+        lstm2 = tf.keras.layers.LSTM(units=128, dropout=0.8, return_sequences=False, name='lstm2')(x_lstm)
+        flat2 = tf.keras.layers.Flatten(name='flat2')(lstm2)
+    with tf.name_scope('dnn'):
+        x_dnn = tf.concat(values=[flat2, x_f], axis=1)
+        x_fc1 = tf.keras.layers.Dense(units=100, activation=tf.nn.relu, use_bias=True,
+                                      kernel_initializer=tf.keras.initializers.TruncatedNormal,
+                                      bias_initializer=tf.keras.initializers.TruncatedNormal, name='x_fc1')(x_dnn)
+        x_dpt1 = tf.keras.layers.Dropout(rate=0.2, name='x_dpt1')(inputs=x_fc1, training=is_training)
+        x_fc2 = tf.keras.layers.Dense(units=200, activation=tf.nn.relu, use_bias=True,
+                                      kernel_initializer=tf.keras.initializers.TruncatedNormal,
+                                      bias_initializer=tf.keras.initializers.TruncatedNormal, name='x_fc2')(x_dpt1)
+        x_dpt2 = tf.keras.layers.Dropout(rate=0.2, name='x_dpt2')(inputs=x_fc2, training=is_training)
+        output = tf.keras.layers.Dense(units=11, activation=tf.nn.relu, use_bias=True,
+                                       kernel_initializer=tf.keras.initializers.TruncatedNormal,
+                                       bias_initializer=tf.keras.initializers.TruncatedNormal, name='output')(x_dpt2)
+        output = tf.keras.activations.softmax(x=output)
+    return output
 
 
 
--- hole_model.py
+++ hole_model.py
@@ -13,81 +13,13 @@ import numpy as np
 import os
 from Saving_model_pb import SaveImport_model, use_pb, import_ops
 
-def check(first_check):
-    '''
-    整体分类器
-    :param first_check: 初级分类器
-    :return: 初级分类器, func
-    '''
-    dict1 = {0:7.17, 1:17.93, 2:20}
-    dict2 = {0:0.05, 1:3.26, 2:4.48, 3:23, 4:35, 5:71, 6:107, 7:143, 8:179, 9:215, 10:251}
-    def second_check1():
-        '''
-        第二层分类器1
-        :return: 具体类别
-        '''
-        nonlocal  dict1
-        pb_file_path = os.getcwd()
-        g2 = tf.Graph()
-        with g2.as_default():
-            init = tf.global_variables_initializer()
-            gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
-        with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options), graph=g2) as sess:
-            sess.run(init)
-            use_pb(sess_new=sess, pb_file_path=pb_file_path, file_suffix=r'/second_classifier1')
-            input = np.arange(107) #改
-            x_f = import_ops(sess_new=sess, op_name='placeholder/x_f')
-            x_l = import_ops(sess_new=sess, op_name='placeholder/x_l')
-            is_training = import_ops(sess_new=sess, op_name='placeholder/is_training')
-            output = import_ops(sess_new=sess, op_name='dnn/Softmax')
-            r_classify = sess.run(output, feed_dict={x_f: input[:4][np.newaxis, :], x_l: input[4:-3][np.newaxis, :],
-                                                     is_training: False})
-            r_classify = r_classify.argmax()
-            r_finally = dict1[r_classify]
-        return r_finally
-
-    def second_check2():
-        '''
-        第二层分类器1
-        :return: 具体类别
-        '''
-        nonlocal dict2
-        pb_file_path = os.getcwd()
-        g3 = tf.Graph()
-        with g3.as_default():
-            init = tf.global_variables_initializer()
-            gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
-        with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options), graph=g3) as sess:
-            sess.run(init)
-            use_pb(sess_new=sess, pb_file_path=pb_file_path, file_suffix=r'/second_classifier2')
-            input = np.arange(107)  # 改
-            x_f = import_ops(sess_new=sess, op_name='placeholder/x_f')
-            x_l = import_ops(sess_new=sess, op_name='placeholder/x_l')
-            is_training = import_ops(sess_new=sess, op_name='placeholder/is_training')
-            output = import_ops(sess_new=sess, op_name='dnn/Softmax')
-            r_classify = sess.run(output, feed_dict={x_f: input[:4][np.newaxis, :], x_l: input[4:-3][np.newaxis, :],
-                                                     is_training: False})
-            r_classify = r_classify.argmax()
-            r_finally = dict2[r_classify]
-        return r_finally
-
-    r_classify = first_check()
-    if r_classify == 0:
-        r_finally = 0.01
-    elif r_classify == 1:
-        r_finally = second_check1()
-    else:
-        r_finally = second_check2()
-    print('最优半径为: %s' % r_finally)
-    return first_check
-
-@check
-def first_check():
+def first_check(input):
     '''
     第一层分类器
-    :return: 半径所属大类别, int
+    :param input: 输入特征向量(矩阵)/标签
+    :return: 半径所属大类别, numpy.ndarray
     '''
-    pb_file_path = os.getcwd()
+    pb_file_path = r'/home/xiaosong/桌面/model/full_model'
     g1 = tf.Graph()
     with g1.as_default():
         init = tf.global_variables_initializer()
@@ -95,18 +27,84 @@ def first_check():
     with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options), graph=g1) as sess:
         sess.run(init)
         use_pb(sess_new=sess, pb_file_path=pb_file_path, file_suffix=r'/first_classifier')
-        input = np.arange(107) #改
         x_f = import_ops(sess_new=sess, op_name='placeholder/x_f')
         x_l = import_ops(sess_new=sess, op_name='placeholder/x_l')
         is_training = import_ops(sess_new=sess, op_name='placeholder/is_training')
         output = import_ops(sess_new=sess, op_name='dnn/Softmax')
-        r_classify = sess.run(output, feed_dict={x_f: input[:4][np.newaxis, :], x_l: input[4:-3][np.newaxis, :],
+        r_classify = sess.run(output, feed_dict={x_f: input[:, :4], x_l: input[:, 4:],
                                                  is_training: False})
-        r_classify = r_classify.argmax()
+        r_classify = np.argmax(a=r_classify, axis=1)
     return r_classify
 
+def second_check1(input):
+    '''
+    第二层分类器1
+    :param input: 输入特征向量(矩阵)/标签
+    :return: 具体类别, numpy.ndarray
+    '''
+    dict1 = {0:7.17, 1:17.93, 2:20}
+    pb_file_path = r'/home/xiaosong/桌面/model/full_model_1'
+    g2 = tf.Graph()
+    with g2.as_default():
+        init = tf.global_variables_initializer()
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options), graph=g2) as sess:
+        sess.run(init)
+        use_pb(sess_new=sess, pb_file_path=pb_file_path, file_suffix=r'/second_classifier_1')
+        x_f = import_ops(sess_new=sess, op_name='placeholder/x_f')
+        x_l = import_ops(sess_new=sess, op_name='placeholder/x_l')
+        is_training = import_ops(sess_new=sess, op_name='placeholder/is_training')
+        output = import_ops(sess_new=sess, op_name='dnn/Softmax')
+        r_classify = sess.run(output, feed_dict={x_f: input[:, :4], x_l: input[:, 4:],
+                                                 is_training: False})
+        r_classify = np.argmax(a=r_classify, axis=1)
+        r_finally = dict1[r_classify]
+        return r_finally
+
+def second_check2(input):
+    '''
+    第二层分类器1
+    :param input: 输入特征向量(矩阵)/标签
+    :return: 具体类别, numpy.ndarray
+    '''
+    dict2 = {0:0.05, 1:3.26, 2:4.48, 3:23, 4:35, 5:71, 6:107, 7:143, 8:179, 9:215, 10:251}
+    pb_file_path = r'/home/xiaosong/桌面/model/full_model_2'
+    g3 = tf.Graph()
+    with g3.as_default():
+        init = tf.global_variables_initializer()
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options), graph=g3) as sess:
+        sess.run(init)
+        use_pb(sess_new=sess, pb_file_path=pb_file_path, file_suffix=r'/second_classifier_2')
+        x_f = import_ops(sess_new=sess, op_name='placeholder/x_f')
+        x_l = import_ops(sess_new=sess, op_name='placeholder/x_l')
+        is_training = import_ops(sess_new=sess, op_name='placeholder/is_training')
+        output = import_ops(sess_new=sess, op_name='sub_dnn/Softmax')
+        r_classify = sess.run(output, feed_dict={x_f: input[:, :4], x_l: input[:, 4:],
+                                                 is_training: False})
+        r_classify = np.argmax(a=r_classify, axis=1)
+        r_finally = dict2[r_classify]
+    return r_finally
+
+def check(input):
+    '''
+    整体分类器
+    :param input: 输入特征向量(矩阵)/标签
+    :return: 最优半径矩阵, numpy.ndarray
+    '''
+    r_classify = first_check(input=input) #得到初级分类器的三类别标记, 分别为0, 1, 2
+    if r_classify == 0:
+        r_finally = 0.01
+    elif r_classify == 1:
+        r_finally = second_check1(input=input)
+    else:
+        r_finally = second_check2(input=input)
+    print('最优半径为: %s' % r_finally)
+    return r_finally
+
 if __name__ == '__main__':
-    pass
+    input = np.arange(104)[np.newaxis, :]
+    r_finally = check(input=input)
 
 
 
