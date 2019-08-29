2f4c5bd2936409b63a7999067b53902cde29475c
xiaosonggege <1243049371@qq.com>
2019-08-29 21:11:12 +0800

    加入了三个模型整体对最优半径的预测模型组合

--- dataset_submaking.py
+++ dataset_submaking.py
@@ -2,7 +2,6 @@
 # encoding: utf-8
 '''
 @author: songyunlong
-@license: (C) Copyright 2018-2021, Node Supply Chain Manager Corporation Limited.
 @contact: 1243049371@qq.com
 @software: Pycharm
 @file: dataset_submaking
